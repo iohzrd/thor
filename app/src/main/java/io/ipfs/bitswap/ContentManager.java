20bd2c83a87c50c192664b0ad43def438e31b8af
remmerw <remmer.wilts@gmail.com>
2021-03-22 15:49:16 +0100

    Version 0.7.0

--- app/src/main/java/io/ipfs/bitswap/ContentManager.java
+++ app/src/main/java/io/ipfs/bitswap/ContentManager.java
@@ -76,7 +76,7 @@ public class ContentManager {
                     public void Peer(@NonNull String pid) {
                         PeerID peer = new PeerID(pid);
 
-                        if (!wants.contains(peer)) {
+                        if (!wants.contains(peer) && !priority.contains(peer) && !faulty.contains(peer)) {
                             wants.add(peer);
                             WANTS.execute(() -> {
                                 if (searches.contains(cid)) { // check still valid
